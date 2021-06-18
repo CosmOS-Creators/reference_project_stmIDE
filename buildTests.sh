a69eb32ef9833b1269c71aa09a3cf91bf7c8a5a4
PavolKostolansky <pavol.kostolansky@gmail.com>
2021-06-18 18:18:40 +0200

    fully automated testing + coverage generating

--- buildTests.sh
+++ buildTests.sh
@@ -1,5 +1,12 @@
-#run from the reference project root
-
 cmake -S . -B Cosmos/generated/build/tests
-cd /mnt/c/GIT/STM32/reference_project_stmIDE/Cosmos/generated/build/tests && make -j32
+cd /mnt/c/GIT/STM32/reference_project_stmIDE/Cosmos/generated/build/tests && make -j32 CPPUTEST_USE_GCOV=Y
 ctest -V
+find . -type d -wholename "*/__/__/__/src*" > tmp.txt
+args="--capture"
+while IFS= read -r line; do
+    args+=" --directory $line"
+done < tmp.txt
+rm tmp.txt
+args+=" --output-file coverage.info"
+lcov ${args}
+genhtml coverage.info --output-directory coverage --rc genhtml_branch_coverage=1
