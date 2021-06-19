ee577e5e6753a894ab4ec014aa0df33fbfc0187b
florianL21 <flo.laschober@gmail.com>
2021-06-19 17:23:01 +0200

    Storing test and coverage results as an artifact

--- buildTests.sh
+++ buildTests.sh
@@ -2,12 +2,12 @@ cmake -S . -B Cosmos/generated/build/tests
 cd Cosmos/generated/build/tests && make -j32 CPPUTEST_USE_GCOV=Y
 ctest -V
 find . -type d -wholename "*/__/__/__/src*" > tmp.txt
-find . -type f -name "*.xml*" > results.txt
 args="--capture"
 while IFS= read -r line; do
     args+=" --directory $line"
 done < tmp.txt
 rm tmp.txt
-args+=" --output-file coverage.info"
+args+=" --output-file results/coverage/coverage.info"
+mkdir results/coverage
 lcov ${args}
-genhtml coverage.info --output-directory coverage --rc genhtml_branch_coverage=1
+genhtml results/coverage/coverage.info --output-directory results/coverage --rc genhtml_branch_coverage=1
