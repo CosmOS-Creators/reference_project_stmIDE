716d1537f6882b66d4fc259c387a82acc9ba1a4f
PavolKostolansky <pavol.kostolansky@gmail.com>
2021-11-03 18:23:46 +0000

    builds fixed - system and tests, now also generating output to the correct cosmos/generated/build directories

--- Cosmos/build/tests/build.sh
+++ Cosmos/build/tests/build.sh
@@ -1,5 +1,5 @@
-cmake -S . -B Cosmos/generated/build/tests
-cd Cosmos/generated/build/tests && make -j32 CPPUTEST_USE_GCOV=Y
+cd .. && cmake -G "Unix Makefiles" -S . -B ../generated/build/tests -DBUILD_TESTS=1
+cd ../generated/build/tests && make -j32 CPPUTEST_USE_GCOV=Y
 ctest -V
 find . -type d -wholename "*/__/__/__/src*" > tmp.txt
 args="--capture"
@@ -8,6 +8,7 @@ while IFS= read -r line; do
 done < tmp.txt
 rm tmp.txt
 args+=" --output-file results/coverage/coverage.info"
+mkdir results
 mkdir results/coverage
 lcov ${args}
 genhtml results/coverage/coverage.info --output-directory results/coverage --rc genhtml_branch_coverage=1
